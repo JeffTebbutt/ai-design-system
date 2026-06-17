a3a61e29a1f38a2d901652361c001d73edbe0d98
JeffTebbutt <jeff.tebbutt@one.nz>
2026-06-17 13:26:08 +1200

    Add Button component using tokens

---
+++ components/Button/Button.tsx
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import tokens from "../../tokens/tokens.json";
+
+function resolveToken(name: string) {
+  return (tokens as any[]).find((t) => t.name === name)?.value;
+}
+
+export function Button() {
+  const background = resolveToken("color.primary");
+
+  return (
+    <button
+      style={{
+        backgroundColor: background,
+      }}
+    >
+      Button
+    </button>
+  );
+}
