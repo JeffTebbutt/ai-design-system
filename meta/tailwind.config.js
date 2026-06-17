7b0348eaf4fd159bfa35a4e9b97fc84095905948
JeffTebbutt <jeff.tebbutt@one.nz>
2026-06-17 14:22:29 +1200

    Add Tailwind config derived from tokens

---
+++ meta/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "surface-primary": "#1b7c53",
+        "on-surface-primary": "#ffffff"
+      },
+      spacing: {
+        "large": "24px"
+      },
+      borderRadius: {
+        "full": "1000px"
+      },
+      height: {
+        "300": "48px"
+      },
+      minWidth: {
+        "500": "80px"
+      }
+    }
+  }
+};
