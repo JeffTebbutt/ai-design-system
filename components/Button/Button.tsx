3f20da3428376b2228d69d4530b59562bee40c1b
JeffTebbutt <jeff.tebbutt@one.nz>
2026-06-17 21:23:03 +1200

    Updated button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,6 +1,12 @@
-export function Button({ children }) {
+import { ReactNode } from "react";
+
+type ButtonProps = {
+  children: ReactNode;
+};
+
+export function Button({ children }: ButtonProps) {
   return (
-    <button className="">
+    <button className="px-large h-7xl min-w-9xl rounded-full bg-surface-primary text-on-surface-primary hover:bg-surface-primary-weak">
       {children}
     </button>
   );
