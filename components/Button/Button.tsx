773956c031cfc22c4424e8646b79e66309e9107e
JeffTebbutt <jeff.tebbutt@one.nz>
2026-06-17 13:37:45 +1200

    Add Button token contract

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,12 +1,13 @@
 // @ts-nocheck
 import tokens from "../../tokens/tokens.json";
+import tokenMap from "./Button.tokens.json";
 
 function resolveToken(name: string) {
   return (tokens as any[]).find((t) => t.name === name)?.value;
 }
 
 export function Button() {
-  const background = resolveToken("color.primary");
+  const background = resolveToken(tokenMap.background);
 
   return (
     <button
